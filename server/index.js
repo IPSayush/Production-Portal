2623ac07e7208273173437833f2635c6f8418626
ayush8627 <pnchalprakashan@gmail.com>
2026-05-24 00:41:57 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const sheetRoutes = require('./routes/sheets');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: '*' }));
+// app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {

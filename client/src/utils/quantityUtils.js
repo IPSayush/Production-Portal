389c3300146570d02ae91a67c14469bbe4e3e902
ayush8627 <pnchalprakashan@gmail.com>
2026-05-30 20:04:53 +0530

    initial commit

--- client/src/utils/quantityUtils.js
+++
@@ -1,7 +0,0 @@
-export function calculateQuantityTotal(rows) {
-  return (rows || []).reduce((sum, row) => sum + (Number(row.quantity) || 0), 0);
-}
-
-export function formatQuantityTotal(total) {
-  return total.toLocaleString('en-IN');
-}
